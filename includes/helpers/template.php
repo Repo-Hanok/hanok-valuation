d5e30ce8c676ab9297cdd8d7bd245659b256902d
Said <operations@hanokagency.com>
2026-01-13 13:05:41 +0100

    producción está ahora mismo así --->

--- includes/helpers/template.php
+++ includes/helpers/template.php
@@ -1,29 +1,29 @@
-<?php
-
-
-
-function hanok_render($template, array $vars = []) {
-
-    $template_path = plugin_dir_path( __FILE__ ) . '../../templates/' . $template;
-
-    if (!file_exists($template_path)) {
-
-        return '';
-
-    }
-
-
-
-    extract($vars);
-        // 📌 DEBUG: muestra TODO lo que llega al render
-    //error_log("HANOK_RENDER VARS: " . print_r($vars, true));
-
-
-
-    ob_start();
-
-    include $template_path;
-
-    return ob_get_clean();
-
+<?php
+
+
+
+function hanok_render($template, array $vars = []) {
+
+    $template_path = plugin_dir_path( __FILE__ ) . '../../templates/' . $template;
+
+    if (!file_exists($template_path)) {
+
+        return '';
+
+    }
+
+
+
+    extract($vars);
+        // 📌 DEBUG: muestra TODO lo que llega al render
+    //error_log("HANOK_RENDER VARS: " . print_r($vars, true));
+
+
+
+    ob_start();
+
+    include $template_path;
+
+    return ob_get_clean();
+
 }
