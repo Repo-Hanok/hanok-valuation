51a1cff62e836649c568e236db934fbc7965e98a
Said <operations@hanokagency.com>
2026-05-26 13:40:41 +0200

    recuperamos el código en prod. Para hacer cambios y refactor sobre lo que hay hecho y funcionando

--- hanok-valuation.php
+++ hanok-valuation.php
@@ -18,34 +18,29 @@
 
 // constantes
 define( 'HANOK_PLUGIN_PATH', plugin_dir_path( __FILE__ ) ); // ruta relativa para 'includes' (servidor)
-
 define( 'HANOK_PLUGIN_URL', plugin_dir_url( __FILE__ ) ); // ruta absoluta para 'enqueues' (navegador)
 
 // accesos
 define('CASSANDRA_URL', 'https://api.cassandra-ai.com/api/valuation/avm');
-
 define('CASSANDRA_KEY', 'IRxQYM6q1E2yH8S6L0AZAD6yaqWqxBgPiIeMbVFacSJXSTzelDv79sMiNTGO6S87l6k');
-
 define('ACTIVE_CAMPAIGN_URL', 'https://xpxfhvfx1l.execute-api.eu-west-3.amazonaws.com/mi-etapa/mi-recurso');
-
 define('AWS_URL_LEGACY', 'https://ne30b426id.execute-api.eu-west-3.amazonaws.com/vv-etapa-api');
 
+/**
+ * nuevas variables para Twilio OTP
+ */
+define('TWILIO_API_KEY', 'SK97e2593f49f28fcbd984b28d56590db8');
+define('TWILIO_API_SECRET', 'i5Bvy7AXyu0V21t5pZ7JbO468Q1lIxIS');
+define('TWILIO_VERIFY_SERVICE_SID', 'VAb28f104280e4726261b1d0677298611f');
+
 require_once HANOK_PLUGIN_PATH . 'includes/shortcode-form.php';
-
 require_once HANOK_PLUGIN_PATH . 'includes/shortcode-valoracion.php';
-
 require_once HANOK_PLUGIN_PATH . 'includes/process-form.php';
-
 require_once HANOK_PLUGIN_PATH . 'includes/helpers/valuation-utils.php';
-
 require_once HANOK_PLUGIN_PATH . 'includes/helpers/template.php';
-
 require_once HANOK_PLUGIN_PATH . 'includes/helpers/active-campaign-api.php';
-
 require_once HANOK_PLUGIN_PATH . 'includes/helpers/cassandra-api.php';
-
 require_once HANOK_PLUGIN_PATH . 'includes/helpers/phone-send-otp.php';
-
 require_once HANOK_PLUGIN_PATH . 'includes/helpers/phone-verify-otp.php';
 
 
